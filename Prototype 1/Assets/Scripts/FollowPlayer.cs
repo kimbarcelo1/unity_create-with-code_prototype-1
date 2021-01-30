13aaf3304683b6fe838b9d4aa9830c4d160abd3f
Kim Barcelo <kim.barcelo@nbs.systems>
2021-01-30 18:49:12 +0800

    Completed Prototype 1 and the Challenge.

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // public and private are access modifiers
+    public GameObject player;
+    private Vector3 vector3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vector3 = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // position ng camera = player position + yung original position ng camera para hindi mapunta sa 0,0,0 si camera
+        //transform.position = player.transform.position + new Vector3(0, 5, -7); //new Vector3 is temporary 
+        transform.position = player.transform.position + vector3;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get player input
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        // move forward and backwards
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveVertical);
+        
+        // rotate left and right
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveHorizontal);
+    }
+}
