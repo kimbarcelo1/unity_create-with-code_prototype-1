1e3f0b0c36b50329282d57bf0a7d47be3e7b0908
Kim Barcelo <kim.barcelo@nbs.systems>
2021-02-07 17:15:03 +0800

    Added a prototype for speedometer and rpm

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] protected float speed;
+    [SerializeField] private float horsePower = 0.0f;
+    [SerializeField] float speed;
+    [SerializeField] float rpm;
     [SerializeField] private float turnSpeed;
     private float moveHorizontal;
     private float moveVertical;
-  
+
+    private Rigidbody rb;
+    [SerializeField] GameObject centerOfMass;
+
+    [SerializeField] TextMeshProUGUI speedText;
+    [SerializeField] TextMeshProUGUI rmpText;
+
+    [SerializeField] List<WheelCollider> allWheels;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.position;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -17,9 +34,41 @@ public class PlayerController : MonoBehaviour
         moveVertical = Input.GetAxis("Vertical");
 
         // move forward and backwards
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveVertical);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * moveVertical);
+        if (IsOnGround())
+        {
+            rb.AddRelativeForce(Vector3.forward * horsePower * moveVertical);
+
+            // rotate left and right
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveHorizontal);
+
+            speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
+            speedText.SetText("Speed: " + speed + " kph");
+
+            rpm = Mathf.Round((speed % 30) * 40);
+            rmpText.SetText("RPM: " + rpm);
+        }
         
-        // rotate left and right
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveHorizontal);
+    }
+
+    bool IsOnGround()
+    {
+        int wheelsOnGround = 0;
+        foreach(WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if(wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
